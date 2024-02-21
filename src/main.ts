c9c02781dd1ead60d3b3fc6fe45377350dd2efcb
FarisCareem <faris.ahmed@ext.careem.com>
2024-02-22 00:21:59 +0400

    added cors support and simulating slow response

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'verbose', 'debug', 'fatal'],
+    cors: true,
   });
   await app.listen(3005);
 }
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -6,7 +6,8 @@ export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger(`HTTP`);
   use(req: Request, res: Response, next: NextFunction) {
     this.logger.log(`Logging HTTP request ${req.method} ${req.originalUrl} ${res.statusCode}`,);
-    next();
+    
+    setTimeout(next, 1500); // Simulate slow response
   }
 }
 
